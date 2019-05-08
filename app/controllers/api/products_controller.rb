d45afeb2d95dc384d228be6f54bf30209afcb8ea
Kelly <kellybucheger@gmail.com>
2019-05-08 15:10:58 -0400

    adds all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,8 +1,22 @@
 class Api::ProductsController < ApplicationController
   def all_products_action
-    @products = Products.all
+    @products = Product.all
     render "all_products.json.jbuilder"
   end
 
-  
+  def garlic_press_action
+    render "garlic_press.json.jbuilder"
+  end
+
+  def cheese_grater_action
+    render "cheese_grater.json.jbuilder"
+  end
+
+  def grill_action
+    render "grill.json.jbuilder"
+  end
+
+  def blender
+    render "blender.json.jbuilder"
+  end
 end
--- app/views/api/products/all_products.json.jbuilder
+++ app/views/api/products/all_products.json.jbuilder
@@ -1,1 +1,7 @@
-json.message "hello"
+json.array! @products.each do |product|
+  json.id product.id
+  json.name product.name
+  json.price product.price
+  json.image  product.image_url
+  json.description product.description
+end
