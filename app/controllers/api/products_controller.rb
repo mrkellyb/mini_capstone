b84214123811fd924cd05e3fe638d2ac06d710c2
Kelly <kellybucheger@gmail.com>
2019-05-08 14:54:04 -0400

    adds routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,8 @@
 class Api::ProductsController < ApplicationController
+  def all_products_action
+    @products = Products.all
+    render "all_products.json.jbuilder"
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+
+  namespace :api do
+    get "/all_products_url" => "products#all_products_action"
+
+    get "/garlic_press_url" => "products#garlic_press_action"
+
+    get "/cheese_grater_url" => "products#cheese_grater_action"
+
+    get "/grill_url" => "products#grill_action"
+    
+    get "/blender_url" => "products#blender_action"
+  end
 end
